0c85855921613220de380e77f37acaeaa0190488
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-08-31 04:41:31 +0000

    Refactor: Comments

--- datavis/src/App.js
+++ datavis/src/App.js
@@ -1,13 +1,14 @@
 import ScatterChart from "./components/ScatterChart";
 import BarChart from "./components/BarChart";
 
-// ScatterChart: plot drawn between "Color Intensity"
-// on the horizontal axis and "Hue" on the vertical axis
-
-// BarChart: plot drawn between "Alcohol"
-// on the horizontal axis and "malic acid" on the vertical axis
-
-function App() {
+/**
+ * ScatterChart: plot drawn between "Color Intensity"
+ * on the horizontal axis and "Hue" on the vertical axis
+ * 
+ * BarChart: plot drawn between "Alcohol"
+ * on the horizontal axis and "Malic Acid" on the vertical axis
+ */
+const App = () => {
   return (
     <>
       <ScatterChart/>
